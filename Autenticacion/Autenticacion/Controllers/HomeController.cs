edb4263640aa51b5f5795f2b5f7856a44d496683
efabian1989 <edwin.fabian.mosquea@gmail.com>
2019-03-11 15:41:44 -0400

    Se crearon varios usuarios para el login

---
+++ Autenticacion/Autenticacion/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Autenticacion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Autenticacion.Controllers
+{
+   // [Authorize(Roles ="Administrador,Vendedor")]
+    public class HomeController : Controller
+    {
+       // [AllowAnonymous]
+        public ActionResult Index()
+        {
+            var autenticado = User.Identity.IsAuthenticated;
+            if (autenticado) {
+                var nombre = User.Identity.Name;
+                var id = User.Identity.GetUserId();
+            using(ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var usuario =db.Users.FirstOrDefault(x=>x.Id==id);
+                    var email = usuario.Email;
+                    var confimacion =usuario.EmailConfirmed;
+
+                    var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+                   //var usuario2= userManager.FindById(id);
+                    var user = new ApplicationUser();
+                    user.UserName = "edwinfabian";
+                    user.Email = "efabian1989@gmail.com";
+
+                    //crear usuario
+
+                    var resultado = userManager.Create(user, "MiPasswordSecreto");
+               
+            }
+            }
+            return View();
+         }
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Autenticacion/Autenticacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Autenticacion.MvcApplication" Language="C#" %>
