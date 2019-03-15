11fc5b982b1f521a249f4f056da34de97d38539a
efabian1989 <edwin.fabian.mosquea@gmail.com>
2019-03-15 16:36:12 -0400

    Se añadio proyecto de GoogleAuthenticator

---
+++ GoogleAuthentication/GoogleAuthentication/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Google.Authenticator;
+using GoogleAuthentication.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleAuthentication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+
+        {
+            if (User.Identity.IsAuthenticated) {
+                var id = User.Identity.GetUserId();
+                TwoFactorAuthenticator autenticador = new TwoFactorAuthenticator();
+                var setupInfo = autenticador.GenerateSetupCode("GoogleAuthentication", User.Identity.Name, id, 300, 300);
+
+                ViewBag.qrCodeImageUrl = setupInfo.QrCodeSetupImageUrl;
+                ViewBag.manualEntrySetupCode = setupInfo.ManualEntryKey;
+
+                bool pinCorrecto = autenticador.ValidateTwoFactorPIN(id, "123456");
+
+
+                // ApplicationDbContext db = new ApplicationDbContext();
+
+
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GoogleAuthentication/GoogleAuthentication/Migrations/201903151919283_AdicionColumna.cs
@@ -0,0 +1,18 @@
+namespace GoogleAuthentication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdicionColumna : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "UsarGoogleAuthenticator", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "UsarGoogleAuthenticator");
+        }
+    }
+}
