2f262ce8ce40e7b5e55c09d26bc0f8f7da18d0bf
efabian1989 <edwin.fabian.mosquea@gmail.com>
2019-03-16 11:56:23 -0400

    Se agrego proyecto para probar

---
+++ AutenticacionGoogle/AutenticacionGoogle/GoogleAuthenticatorTokenProvider.cs
@@ -0,0 +1,35 @@
+﻿using AutenticacionGoogle.Models;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Google.Authenticator;
+
+namespace AutenticacionGoogle
+{
+    public class GoogleAuthenticatorTokenProvider : IUserTokenProvider<ApplicationUser, string>
+    {
+        public Task<string> GenerateAsync(string purpose, UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            return Task.FromResult("");
+        }
+
+        public Task<bool> IsValidProviderForUserAsync(UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            return Task.FromResult(user.UsarGoogleAuthenticator);
+            
+        }
+
+        public Task NotifyAsync(string token, UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            return Task.FromResult(0);
+        }
+
+        public Task<bool> ValidateAsync(string purpose, string token, UserManager<ApplicationUser, string> manager, ApplicationUser user)
+        {
+            TwoFactorAuthenticator autenticador = new TwoFactorAuthenticator();
+            var resultado =autenticador.ValidateTwoFactorPIN(user.Id, token);
+            return Task.FromResult(resultado);
+
+        }
+    }
+}
