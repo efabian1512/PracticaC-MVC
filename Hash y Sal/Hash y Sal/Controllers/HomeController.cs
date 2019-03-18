946faca95c822235835c73098be5735666c71c03
efabian1989 <edwin.fabian.mosquea@gmail.com>
2019-03-18 10:39:44 -0400

    Se creo arcivho secure app settings para comprobrar no se guarde en git

--- Hash y Sal/Hash y Sal/Controllers/HomeController.cs
+++ Hash y Sal/Hash y Sal/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Helpers;
+using System.Configuration;
 
 namespace Hash_y_Sal.Controllers
 {
@@ -11,14 +12,19 @@ namespace Hash_y_Sal.Controllers
     {
         public ActionResult Index()
         {
-            var sinSal1 = Crypto.Hash("hola");
-            var sinSal2 = Crypto.Hash("hola");
-            var sinSal3 = Crypto.Hash("Hola");
-            var iguales = sinSal1 == sinSal2;
+            //var sinSal1 = Crypto.Hash("hola");
+            //var sinSal2 = Crypto.Hash("hola");
+            //var sinSal3 = Crypto.Hash("Hola");
+            //var iguales = sinSal1 == sinSal2;
 
-            var conSal1 = Crypto.HashPassword("hola");
-            var conSal2 = Crypto.HashPassword("hola");
-            var iguales2 = conSal1 == conSal2;
+            //var conSal1 = Crypto.HashPassword("hola");
+            //var conSal2 = Crypto.HashPassword("hola");
+            //var iguales2 = conSal1 == conSal2;
+
+            var dato1 = ConfigurationManager.AppSettings["Dato1"];
+            var dato2 = ConfigurationManager.AppSettings["Dato2"];
+
+
             return View();
         }
 
