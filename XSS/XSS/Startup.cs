742e326397ff911efbaf4991f8178af7e8f1fd00
efabian1989 <edwin.fabian.mosquea@gmail.com>
2019-03-19 12:11:57 -0400

    Se practico con la vulverabilidad XSS

---
+++ XSS/XSS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XSS.Startup))]
+namespace XSS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
